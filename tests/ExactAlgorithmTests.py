38bf75627f6b1b62714b7821d683a846e503e205
radek <fffffrygio@gmail.com>
2024-05-23 17:37:04 +0200

    use good solution

--- tests/ExactAlgorithmTests.py
+++ tests/ExactAlgorithmTests.py
@@ -1,21 +1,22 @@
 import unittest
 
-from Sudoku import get_random_sudoku, GOOD_SOLUTION
+from Sudoku import get_random_sudoku
 from exact_algorithm import ExactAlgorithm
+from good_solution_source import get_good_solution
 
 
 class ExactAlgorithmTests(unittest.TestCase):
     def setUp(self):
-        self.good_solution = GOOD_SOLUTION.copy()
+        self.good_solution = get_good_solution()
 
     def test_exact_algorithm(self):
-        sudoku = get_random_sudoku(8, self.good_solution)
+        sudoku = get_random_sudoku(5, self.good_solution)
 
         exact_algorithm = ExactAlgorithm(sudoku)
 
         result_sudoku = exact_algorithm.exact_algorithm()
 
-        self.assertEqual(sudoku.count_mistakes(), 0)
+        self.assertEqual(result_sudoku.count_mistakes(), 0)
 
 
 if __name__ == '__main__':
