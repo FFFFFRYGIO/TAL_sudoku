1f7aae6c9f2b94933a6a9b64bd042ba6191301df
radek <fffffrygio@gmail.com>
2024-05-24 01:47:02 +0200

    parametrize test_genetic_algorithm

--- tests/GeneticAlgorithmTests.py
+++ tests/GeneticAlgorithmTests.py
@@ -1,18 +1,24 @@
 import unittest
+from itertools import product
+
+from ddt import ddt, data, unpack
 
 from Sudoku import get_random_sudoku
 from genetic_algorithm import GeneticAlgorithm
 from good_solution_source import get_good_solution
 
 
+@ddt
 class GeneticAlgorithmTests(unittest.TestCase):
     def setUp(self):
         self.good_solution = get_good_solution()
 
-    def test_genetic_algorithm(self):
-        sudoku = get_random_sudoku(10, self.good_solution)
+    @data(*product(range(10, 11), [5, 10, 20]))
+    @unpack
+    def test_genetic_algorithm(self, num_of_empty_cells=5, population_number=10):
+        sudoku = get_random_sudoku(num_of_empty_cells, self.good_solution)
 
-        genetic_algorithm = GeneticAlgorithm(sudoku, population_number=10)
+        genetic_algorithm = GeneticAlgorithm(sudoku, population_number=population_number)
 
         result_sudoku = genetic_algorithm.genetic_algorithm()
 
